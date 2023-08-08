7c9f7798ce16d07d9c3c50b74222036365eb58d0
jh2277 <96563287+jh2277@users.noreply.github.com>
2023-08-08 11:41:51 +0900

    혜빈이꺼 추가

--- Assets/0Scenes/hyebeen/Timer.cs
+++ Assets/0Scenes/hyebeen/Timer.cs
@@ -5,36 +5,45 @@ using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-    public float timeValue = 90;
+    public float timeValue = 10;
     public Text timerText;
-
+    public GameObject gameOverText; // Reference to the Game Over text object
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Hide the Game Over text initially
+        gameOverText.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(timeValue > 0){
+        if (timeValue > 0)
+        {
             timeValue -= Time.deltaTime;
-        }else{
-            timeValue=0;
         }
-        
+        else
+        {
+            timeValue = 0;
+
+            // Show the Game Over text
+            gameOverText.SetActive(true);
+        }
+
         DisplayTime(timeValue);
     }
 
-    void DisplayTime(float timeToDisplay){
-        if(timeToDisplay<0){
-            timeToDisplay=0;
+    void DisplayTime(float timeToDisplay)
+    {
+        if (timeToDisplay < 0)
+        {
+            timeToDisplay = 0;
         }
 
-        float minutes = Mathf.FloorToInt(timeToDisplay/60);
-        float seconds = Mathf.FloorToInt(timeToDisplay%60);
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
 
-        timerText.text=string.Format("{0:00}:{1:00}",minutes,seconds);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 }
